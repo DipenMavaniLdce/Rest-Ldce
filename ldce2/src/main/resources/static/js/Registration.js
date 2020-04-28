bebd06dc9e70ff8f535cc534c1cea267e425ba63
DipenMavaniLdce <62199316+DipenMavaniLdce@users.noreply.github.com>
2020-04-28 12:16:51 +0530

    All css,js and html

--- ldce2/src/main/resources/static/js/Registration.js
+++ ldce2/src/main/resources/static/js/Registration.js
@@ -1,45 +1,44 @@
 function AjexSubmit(){
-var url ;
-var data;
-if($('#regForm').length){
-	 var form = $('#regForm')[0];
- 
-     var data = new FormData(form);
-    url= "../registerStudent"
-}
-else if($('#editProfileForm').length){
-			var form = $('#editProfileForm')[0];
-	 		var data = new FormData(form);
-    		url= "../student/updateStudent"
-}
-else
-{
-	alert("some thing went wrong")
+
+    // Get form
+       
+    var url ;
+    var data;
+
+    if($('#regForm').length) {
+        var form = $('#regForm')[0];
+    
+        var data = new FormData(form);
+        url= "/registerStudent"
+    }
+    else if($('#editProfileForm').length) {
+        var form = $('#editProfileForm')[0];
+        var data = new FormData(form);
+        url= "/student/updateStudent"
+    }
+    else {
+	    alert("some thing went wrong")
 	}
 
-      
+    $.ajax({
+        type: "POST",
+        enctype: 'multipart/form-data',
+        url: url,
+        data: data,
+        processData: false,
+        contentType: false,
+        cache: false,
+        timeout: 60000,
+        success: function (data) {
 
-        $.ajax({
-            type: "POST",
-            enctype: 'multipart/form-data',
-            url: url,
-            data: data,
-            processData: false,
-            contentType: false,
-            cache: false,
-            timeout: 60000,
-            success: function (data) {
+            alert("data submitted");
+            window.location = '/student/'; 
 
-              alert("data submitted");
-              window.location = 'http://localhost:8080/student/'; 
+        },
+        error: function (e) {
 
-            },
-            error: function (e) {
-
-            	  window.location = 'http://localhost:8080/registerStudent'; 
-            }
-        });
-
- 
+                window.location = '/registerStudent'; 
+        }
+    });
 
 }
